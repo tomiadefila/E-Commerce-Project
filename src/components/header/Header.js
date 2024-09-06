56fe4ac92ebe42b6950c957faa347d31dd1159bd
tomiadefila <tomiadefila@yahoo.co.uk>
2024-09-06 19:40:17 +0100

    adding changes to list.js

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,29 +1,27 @@
 import "./Header.css";
-import {Link} from "react-router-dom";
-import Logo from "../images/logo192.png"
-
+import { Link } from "react-router-dom";
+import Logo from "../images/logo192.png";
 
 function Header() {
-    return (
-        
+  return (
+    <header>
       <nav>
         <div className="navgrid1">
           <p>SHIPPING TO THE UNITED KINGDOM</p>
           <p>THE VAULT IS OPEN-SHOP NOW</p>
-          <p>SAVED     LOGIN</p>
+          <p>SAVED LOGIN</p>
         </div>
 
-               
         <div className="navgrid2">
           <img src={Logo} className="logo" alt="My website logo" />
-        
+
           <div className="dropdown">
             <Link to="/shop">SHOP</Link>
             <div className="dropdown-content">
-            <Link to="/shop/clothing">Clothing</Link>
-            <Link to="/shop/bags">Bags</Link>
-            <Link to="/shop/shoes">Shoes</Link>
-            <Link to="/shop/accesories">Accesories</Link>
+              <Link to="/shop/clothing">Clothing</Link>
+              <Link to="/shop/bags">Bags</Link>
+              <Link to="/shop/shoes">Shoes</Link>
+              <Link to="/shop/accesories">Accesories</Link>
             </div>
           </div>
           <div>
@@ -33,9 +31,8 @@ function Header() {
             <Link to="/new">NEW</Link>
           </div>
         </div>
-        
       </nav>
-      
-    );
-  }
-  export default Header;
+    </header>
+  );
+}
+export default Header;
